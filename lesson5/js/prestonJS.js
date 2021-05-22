6d83ddce6f81f0f564b913f0f502aab334cb2238
jennelie-hagerty <jenneliehagerty@byui.edu>
2021-05-22 17:46:31 -0400

    preston-5 update 2

--- lesson5/js/prestonJS.js
+++ lesson5/js/prestonJS.js
@@ -13,3 +13,44 @@ window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('
 
   classList property - https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
   */
+
+
+  window.onload = currentdate;
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function currentdate() {
+  //Retrieve date from user's computer
+  var d = new Date();
+  var day = days[d.getDate()];
+  var month = months[d.getMonth()];
+  var year = d.getFullYear();
+
+  let fulldate = `${day} "," ${month}  " " ${d.getDate()} "," ${year}`;
+
+  document.querySelector("#currentdate").textContent = fulldate;
+
+  document.querySelector("#currentdate").innerHTML = document.lastModified;
+}
