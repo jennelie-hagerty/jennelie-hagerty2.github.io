6366b5ae440f2110f9d4a17f20f9274fbea0df2a
jennelie-hagerty <jenneliehagerty@byui.edu>
2021-05-24 19:26:59 -0400

    fixes

--- lesson5/js/prestonJS.js
+++ lesson5/js/prestonJS.js
@@ -1,10 +1,10 @@
 const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
+const mainnav = document.querySelector('.navigation');
 
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
 // To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+window.onresize = () => {if (window.innerWidth > 641) mainnav.classList.remove('responsive')};
 
 /*** Programming Notes **************************************
   Arrow Functions - es6 syntactically compact alternative to a regular function expression
